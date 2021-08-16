cb34c2c20efdaae4580612fbcfe3c6de585e49d9
sanqiumeng <75417344+sanqiumeng@users.noreply.github.com>
2021-08-16 10:55:28 +0800

    Update 数据结构之串.cpp

--- 数据结构之串.cpp
+++ 数据结构之串.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <string.h> 
-//BFÀ„∑®
+//BF
 /*int BF(char s[],char t[])
 {
     int i=0,j=0;
@@ -33,7 +33,7 @@ int main()
     printf("%d",x);
     return 0;
 }*/
-//KMPÀ„∑® 
+//KMP
 int next[1000001];
 void get_next(char t[])
 {
